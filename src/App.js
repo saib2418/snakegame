acfb16ea654e8f9d8e6f1579ebadedffbe757710
girlswhocode-sjsu <49962214+saib2418@users.noreply.github.com>
2023-02-16 13:14:20 -0800

    snakegame setup of game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Board from "./Board/Board.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board/Board.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Board.css';
+
+function Board() {
+    return <h1>Hello World!</h1>
+}
+
+export default Board;
