a0841a31f8acbc43967446dcae6387f1ec3eaf11
girlswhocode-sjsu <49962214+saib2418@users.noreply.github.com>
2023-02-17 18:23:51 -0800

    food consumption

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Board.css";
+import { randomIntFromInterval } from "../lib/utils";
 
 class LinkedListNode {
   constructor(value) {
@@ -16,20 +17,60 @@ class SinglyLinkedList {
   }
 }
 
-let BOARD_SIZE = 10;
+class Cell {
+  constructor(value) {
+    const node = new LinkedListNode(value);
+    this.head = node;
+    this.tail = node;
+  }
+}
+
+const BOARD_SIZE = 10;
+
+const Direction = {
+  UP: "UP",
+  RIGHT: "RIGHT",
+  DOWN: "DOWN",
+  LEFT: "LEFT",
+};
 
 const Board = () => {
   const [board, setBoard] = useState(createBoard(BOARD_SIZE));
+  const [foodCell, setFoodcell] = useState(48);
   const [snakeCells, setSnakeCells] = useState(new Set([44]));
   const [snake, setSnake] = useState(new SinglyLinkedList(44));
+  const [direction, setDirection] = useState(Direction.RIGHT);
+
+  useEffect(() => {
+    setInterval(() => {}, 1000);
+
+    window.addEventListener("keydown", (e) => {
+      const newDirection = getDirectionFromKey(e.key);
+      const isValidDirection = newDirection !== "";
+      if (isValidDirection) setDirection(newDirection);
+    });
+  }, []);
+
+  const handleFoodConsumption = () => {
+    const maxPossibleCellValue = BOARD_SIZE * BOARD_SIZE;
+    let nextFoodCell;
+    while (true) {
+      nextFoodCell = randomIntFromInterval(1, maxPossibleCellValue);
+      if (snakeCells.has(nextFoodCell) || foodCell == nextFoodCell) continue;
+      break;
+    }
+    setFoodcell(nextFoodCell);
+  };
   return (
     <div className="board">
       {board.map((row, rowIdx) => (
         <div key={rowIdx} className="row">
-          {row.map((cell, cellIdx) => (
+          {row.map((cellValue, cellIdx) => (
             <div
               key={cellIdx}
-              className={`cell ${false ? "food-cell" : ""}`}
+              className={`cell ${
+                snakeCells.has(cellValue) ? "snake-cell" : ""
+              }`}
             ></div>
           ))}
         </div>
@@ -50,4 +91,12 @@ const createBoard = (BOARD_SIZE) => {
   }
   return board;
 };
+const getDirectionFromKey = (key) => {
+  if (key == "ArrowUp") return Direction.UP;
+  if (key == "ArrowRight") return Direction.RIGHT;
+  if (key == "ArrowDown") return Direction.DOWN;
+  if (key == "ArrowLeft") return Direction.LEFT;
+  return "";
+};
+
 export default Board;
